997a5126e44063d9655d9a0ce74cd993c5d69c13
Diego30k-tec <diegoarmandoruiz99@gmail.com>
2026-03-28 16:26:13 -0500

    28-03-2026
    Spring Boot

---
+++ backend/src/main/java/com/senati/bodega_monica/BodegaMonicaApplication.java
@@ -0,0 +1,13 @@
+package com.senati.bodega_monica;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BodegaMonicaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BodegaMonicaApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/senati/bodega_monica/BodegaMonicaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.senati.bodega_monica;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BodegaMonicaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
